430418863c1821c1650729e946bd94fa6018afa8
Helena2018 <helena2020liu@gmail.com>
2022-10-22 12:03:20 +1300

    build each part of component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <Navigation />
+      <Navigation />
       <Logo />
-      <Rank />
+                  {/*<Rank />
       <ImageLinkForm /> */}
     </div>
   );
