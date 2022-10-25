7d4e3f8009fb54d583311190c82d96fed0df7b2b
Helena2018 <helena2020liu@gmail.com>
2022-10-25 14:31:24 +1300

    display detecting face box

--- src/components/ImageRecognition/ImageRecognition.js
+++ src/components/ImageRecognition/ImageRecognition.js
@@ -1,12 +1,12 @@
 import React from "react";
 import './ImageRecognition.css';
 
-const ImageRecognition = ({imgUrl, box}) => {
+const ImageRecognition = ({ imgUrl, box }) => {
   return(
     <div className="center">
-      <div className="absolute ma2">
+      <div className="relative ma2">
         <img id="inputImage" alt="" src={imgUrl} width='500px' height='auto' />
-        <div className="bounding-box" style={{top: box.topRow, right: box.rightCol, bottom: box.bottonRow, left: box.leftCol}}></div>
+        <div className="bounding-box" style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
       </div>
     </div>
   )
